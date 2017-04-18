2b5c1ebf0fe5a4655236d005b1a711fae5062a10
HemingWu <jianmo_o@163.com>
2017-04-18 22:38:51 +0800

    项目初始化

---
+++ src/main/java/top/joylife/document/controller/SystemController.java
@@ -0,0 +1,54 @@
+package top.joylife.document.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import top.joylife.document.common.consts.ReData;
+import top.joylife.document.common.util.ReUtil;
+
+import java.util.Date;
+
+
+/**
+ * Created by HemingWu on 2016/10/22.
+ */
+@Controller
+@RequestMapping("/")
+public class SystemController {
+
+    private Logger logger = LoggerFactory.getLogger(SystemController.class);
+
+
+    @RequestMapping("")
+    public String index(ModelMap model){
+        model.put("msg","welcome");
+
+        return "index";
+    }
+
+    @RequestMapping("json")
+    @ResponseBody
+    public ReData indexJson(){
+        return ReUtil.success();
+    }
+
+    @RequestMapping("login")
+    public String home(ModelMap model){
+        logger.error("dsaadasd__________________________");
+
+
+        model.put("aaa",new java.sql.Timestamp(new Date().getTime()));
+
+
+        return "login";
+    }
+
+
+    @RequestMapping("websocketpage")
+    public String webSocket(){
+        return "websocket";
+    }
+}
